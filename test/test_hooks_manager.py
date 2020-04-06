565f2c3514944c884c32cb4aa1667f79bff75d4b
Liam Deacon <liam.m.deacon@gmail.com>
2020-04-06 09:41:02 +0000

    Added more tests

--- test/test_hooks_manager.py
+++ test/test_hooks_manager.py
@@ -1,1 +1,13 @@
-from metapandas.hooks import manager
+import unittest
+from metapandas.hooks.manager import HooksManager
+
+
+class TestHooksManager(unittest.TestCase):
+    def test_HooksManager(self):
+        mgr = HooksManager()
+
+    def test_HooksManager_apply(self):
+        HooksManager.apply_hooks
+
+    def test_HooksManager_remove(self):
+        HooksManager.remove_hooks
--- test/test_hooks_pandas.py
+++ test/test_hooks_pandas.py
@@ -1,1 +1,22 @@
-from metapandas.hooks import pandas
+from metapandas.hooks.pandas import (
+    pandas_read_with_metadata,
+    pandas_save_with_metadata,
+    PandasMetaDataHooks
+)
+
+def test_install_metadata_hooks():
+    PandasMetaDataHooks.install_metadata_hooks()
+    PandasMetaDataHooks.install_metadata_hooks()
+
+
+def test_uninstall_metadata_hooks():
+    PandasMetaDataHooks.uninstall_metadata_hooks()
+    PandasMetaDataHooks.uninstall_metadata_hooks()
+
+
+def test_pandas_save_with_metadata():
+    pass  # TODO
+
+
+def test_pandas_read_with_metadata():
+    pass  # TODO
